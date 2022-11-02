67147cd51990d3a89f3dd840109540c5d74f2123
jingz <Henryz49@nycstudents.net>
2022-11-01 20:15:23 -0400

    First commit

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,78 @@
-package PACKAGE_NAME;public class LinearEquation {
+import java.lang.Math;
+public class LinearEquation {
+    //variables
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+
+    //constructors
+    public LinearEquation(int x1, int x2, int y1, int y2) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+
+    //methods
+    public String lineInfo() {
+        String Info = "Equation: " + this.equation() + "\n Slope: " + this.slope() +
+                "\n Y-intercept:" + this.yIntercept() + "\n Distance: " + this.distance();
+        return Info;
+    }
+
+    public double distance() {
+        double distanceFormula = Math.sqrt((double) (Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
+        return Math.round(distanceFormula * 100) / 100;
+    }
+
+    public double slope() {
+        return (double) (y2 - y1) / (x2 - x1);
+    }
+
+    public double yIntercept() {
+        return y1 - (this.slope() * x1);
+    }
+
+    public String equation() {
+        int yResult = y2 - y1;
+        int xResult = x2 - x1;
+        String mResult = "";
+        String bResult = "";
+        boolean negY_Inter = false;
+        //Slope
+        //anti-double negative
+        if (xResult < 0 && yResult < 0) {
+            yResult *= -1;
+            xResult *= -1;
+        }
+        if (yResult % xResult != 0) {
+            mResult = yResult + "/" + xResult;
+        }
+        if (yResult % xResult == 0) {
+            mResult = "" + (yResult / xResult);
+        }
+
+        //y-intercept
+
+        //if intercept = 0
+        double tempB_Result = y1 - (this.slope() * x1);
+        if (tempB_Result == 0) {
+            bResult = "0";
+        }
+        else bResult = "" + (y1 - (this.slope() * x1));
+        //if intercept is negative
+        if (tempB_Result < 0) {
+            negY_Inter = true;
+        }
+
+        //actual equation
+        if (negY_Inter == true) {
+            return ("y = " + mResult + "x - " + bResult);}
+        if (bResult == "0") {
+            return "y = " + mResult + "x";}
+        return ("y = " + mResult + "x - " + bResult);
+    }
 }
+
+
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class LinearEquationRunner {
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.util.Scanner;
+public class LinearEquationRunner {
+public static void main(String[]args){
+    Scanner input = new Scanner(System.in);
+    //Inputs and variables
+    System.out.println("Welcome! \n Enter Coordinate 1: ");
+    String coord1 = input.nextLine();
+    System.out.println("Enter Coordinate 2: ");
+    String coord2 = input.nextLine();
+    int comma1 = coord1.indexOf(",");
+    int comma2 = coord2.indexOf(",");
+    int x1 = Integer.parseInt(coord1.substring(1, comma1));
+    int x2 = Integer.parseInt(coord2.substring(1, comma2));
+    int y1 = Integer.parseInt(coord1.substring(comma1 + 2, coord1.length() - 1));
+    int y2 = Integer.parseInt(coord2.substring(comma2 + 2, coord2.length() - 1));
+    if (x1 == x2){
+        System.out.println("The line is vertical, therefore x = " + x1);
+        return;
+    }
+    //method running
+    LinearEquation equation = new LinearEquation(x1, x2, y1, y2);
+    System.out.println(equation.lineInfo());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+}
+
